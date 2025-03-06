81d11e7c77f40c2c2c048a91895b49b85a3ec906
Luiz Henrique <lhenrique181104@gmail.com>
2025-03-06 17:14:21 -0300

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -2,39 +2,46 @@
 #include <locale.h>
 
 
- int partition(int arr[], int low, int high) {
-	 int pivot = arr[high];
-	 int i = (low - 1);
-	 for (int j = low; j <= high - 1; j++) {
-		 if (arr[j] < pivot) {
-			 i++;
-			 swap(arr, i, j);
-		 }
-	 }
-	 swap(arr, i + 1, high);
-	 return (i + 1);
- }
+int partition(int arr[], int low, int high) {
+	int pivot = arr[high];
+	int i = (low - 1);
+	for (int j = low; j <= high - 1; j++) {
+		if (arr[j] < pivot) {
+			i++;
+			swap(arr, i, j);
+		}
+	}
+	swap(arr, i + 1, high);
+	return (i + 1);
+}
 
- void quickSort(int arr[], int low, int high) {
-	 if (low < high) {
-		 int pi = partition(arr, low, high);
-		 quickSort(arr, low, pi - 1);
-		 quickSort(arr, pi + 1, high);
-	 }
- }
+void quickSort(int arr[], int low, int high) {
+	if (low < high) {
+		int pi = partition(arr, low, high);
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
 main(){
 	setlocale(LC_ALL,"Portuguese");
 	
-	int array[25]={10,9,8,7,6,5,4,55,77,88,22,33,45,44,77,8,5,1,4,56,3,2,1};
+	int array[10]=[3, 7, 10, 1, 5, 0, 8, 6, 2, 4];
 	int p = sizeof(array) / sizeof(array[0]);
 	int k;
 	
+	printf("\n Array Desordenado: ");
+	printArray(array,p);
+	
 	quickSort(array,p);
 	
-	for (k =0; k < p; k ++){
-		printf("%i",array[k]);
-	}
-	
+	printf("\n Array Ordenado: ");
+	printArray(array,p);
 	
 }
